4b8435a5bc739795c7f42f1f822ca4eb69b2f0b3
satoshi naruki <maptjgpatmpgg1113@gmail.com>
2021-08-15 06:11:17 +0900

    編集ページ実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,36 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
   def index
     @posts = Post.all
   end
   def show
-    
   end
   def new
     @post = Post.new
   end
   def create
-    Post.create(params[:post])
-    redirect_to new_post_path
+    if @post.save
+      redirect_to posts_path, notice: "投稿しました！"
+    else
+      render :new
+    end
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
   end
   private
   def post_params
     params.require(:post).permit(:content)
   end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
-
