4962f11a7beb9f546cd6333501ac57b163a700b0
satoshi naruki <maptjgpatmpgg1113@gmail.com>
2021-08-15 04:43:05 +0900

    新規投稿ページ実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+  def index
+  end
+  def show
+    
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    Post.create(params[:post])
+    redirect_to new_post_path
+  end
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
+
