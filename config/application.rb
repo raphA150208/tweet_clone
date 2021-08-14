7cf25ce87f34c1d3cc2cf09d6234e2f44c922b51
satoshi naruki <maptjgpatmpgg1113@gmail.com>
2021-08-15 02:58:45 +0900

    土台作成

---
+++ config/application.rb
@@ -0,0 +1,13 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+
+Bundler.require(*Rails.groups)
+
+module TweetClone
+  class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+  end
+end
